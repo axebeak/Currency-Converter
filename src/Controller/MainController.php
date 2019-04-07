4f40d2f047c7bded7d28a4cfed997309aa71a994
d <test@example.com>
2019-04-07 14:28:02 +0300

    Finilized

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,7 +6,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception;
 
